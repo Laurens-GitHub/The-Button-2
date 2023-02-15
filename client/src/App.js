2482bfa2890c436fd69c5c25ab6d68f0c3986e2a
Lauren Edwards <Lauren@LEdwards.co>
2023-02-15 16:32:47 -0500

    Initializing client with finished App.js

---
+++ client/src/App.js
@@ -0,0 +1,88 @@
+import React from "react";
+import "./App.css";
+import axios from "axios";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      soundUrl: "",
+      loaded: false,
+      idle: true,
+      disabled: true,
+    };
+  }
+
+  componentDidMount() {
+    /** Requests soundUrl from API once upon page render */
+
+    axios.get("http://localhost:3001/get").then((response) => {
+      this.setState({
+        soundUrl: response.data,
+        loaded: true,
+        disabled: false,
+      });
+    });
+  }
+
+  loadSound = async () => {
+    /** Requests soundUrl from API and updates button states */
+
+    try {
+      const url = await axios.get("http://localhost:3001/get");
+      this.setState({
+        soundUrl: url.data,
+        loaded: true,
+        disabled: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  initialState = () => {
+    /** Resets the button */
+
+    this.setState({
+      soundUrl: "",
+      loaded: false,
+      idle: true,
+      disabled: true,
+    });
+  };
+
+  handleClick = async (e) => {
+    /** Plays the loaded sound and requests a new random sound to be played next.
+  Disables button until current sound is finished and next sound is ready. */
+
+    try {
+      e.target.disabled = true;
+      const sound = new Audio(this.state.soundUrl);
+      await sound.play();
+
+      sound.addEventListener("ended", async () => {
+        if (!sound.paused === false) {
+          try {
+            const url = await axios.get("http://localhost:3001/get");
+            this.setState({
+              soundUrl: url.data,
+              loaded: true,
+              disabled: false,
+            });
+            e.target.disabled = false;
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    return <button onClick={this.handleClick}>Play Sound</button>;
+  }
+}
+
+export default App;
