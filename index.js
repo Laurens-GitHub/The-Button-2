cfb2834703a3640fa651c1e4ad2d0fcdddb9b749
Lauren Edwards <Lauren@LEdwards.co>
2023-02-17 16:51:27 -0500

    wrote listSounds function to request all file names from S3

--- index.js
+++ index.js
@@ -1,50 +1,67 @@
 import express from "express";
 import cors from "cors";
-// Import required AWS SDK clients and commands for Node.js.
-import { GetObjectCommand } from "@aws-sdk/client-s3";
-import { s3Client } from "./libs/s3Client.js"; // Helper function that creates an Amazon S3 service client module.
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import fetch from "node-fetch";
+// Import required AWS SDK clients and commands for Node.js.
+import { s3Client } from "./libs/s3Client.js";
+import { GetObjectCommand, ListObjectsV2Command } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
+app.use(cors());
+
+const listSounds = async () => {
+  /* Returns a list of the names of all items in the bucket */
+  try {
+    const command = new ListObjectsV2Command({
+      Bucket: "soundsforthebutton2",
+    });
+    const data = await s3Client.send(command);
+    let allSounds = [];
+    let contents = data.Contents;
+    contents.forEach(function (content) {
+      allSounds.push(content.Key);
+    });
+    return allSounds;
+  } catch (err) {
+    console.log("Error fetching mp3s", err);
+  }
+};
 
 function randomChoice(arr) {
   return arr[Math.floor(arr.length * Math.random())];
 }
 
-const options = ['Beullers_A_day_like_this.mp3', 'Firefly_Shooting.mp3', 'Futurama_Good_News!_77.mp3', 'LOTR_Samwise_Potatoes.mp3', 'Rick_and_Morty_Wubba_Lubba_Dub_Dub.mp3']
-
 const getSoundUrl = async () => {
+  /* Returns a signed url for a random sound */
   try {
-      const command = new GetObjectCommand({
-        Bucket: 'soundsforthebutton2',
-        Key: randomChoice(options),
-      });
-      // Create the presigned URL.
-      const signedUrl = await getSignedUrl(s3Client, command, {expiresIn: 3600});
-      const response = await fetch(signedUrl);
-      const url = await response;
-      return await url['url']
-
-
+    const config = new GetObjectCommand({
+      Bucket: "soundsforthebutton2",
+      Key: randomChoice(await listSounds()),
+    });
+    // Create the presigned URL.
+    const signedUrl = await getSignedUrl(s3Client, config, {
+      expiresIn: 3600,
+    });
+    const response = await fetch(signedUrl);
+    const url = response;
+    return url["url"];
   } catch (err) {
-      console.log('Error creating presigned URL', err);
-  };
+    console.log("Error creating presigned URL", err);
+  }
 };
 
 // homepage
-app.get('/', (req, res) => {
-  res.send('Welcome!')
+app.get("/", (req, res) => {
+  res.send("Welcome!");
 });
 
 // fetch sound url
-app.get('/get', async (req, res) => {
-  res.send(String(await getSoundUrl()))
+app.get("/get", async (req, res) => {
+  res.send(String(await getSoundUrl()));
 });
 
 // launch API
-app.listen('3001', () => { })
+app.listen("3001", () => {});
