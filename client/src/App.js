670fb00522f42dbea31917a3564198604f77c095
Lauren Edwards <Lauren@LEdwards.co>
2023-02-22 01:23:20 -0500

    styled button with MUI

--- client/src/App.js
+++ client/src/App.js
@@ -4,82 +4,82 @@ import axios from "axios";
 import Button from "@mui/material/Button";
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     soundUrl: "",
-  //     loaded: false,
-  //     idle: true,
-  //     disabled: true,
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      soundUrl: "",
+      loaded: false,
+      idle: true,
+      disabled: true,
+    };
+  }
 
-  // componentDidMount() {
-  //   /** Requests soundUrl from API once upon page render */
+  componentDidMount() {
+    /** Requests soundUrl from API once upon page render */
 
-  //   axios.get("http://localhost:3001/get").then((response) => {
-  //     this.setState({
-  //       soundUrl: response.data,
-  //       loaded: true,
-  //       disabled: false,
-  //     });
-  //   });
-  // }
+    axios.get("http://localhost:3001/get").then((response) => {
+      this.setState({
+        soundUrl: response.data,
+        loaded: true,
+        disabled: false,
+      });
+    });
+  }
 
-  // loadSound = async () => {
-  //   /** Requests soundUrl from API and updates button states */
+  loadSound = async () => {
+    /** Requests soundUrl from API and updates button states */
 
-  //   try {
-  //     const url = await axios.get("http://localhost:3001/get");
-  //     this.setState({
-  //       soundUrl: url.data,
-  //       loaded: true,
-  //       disabled: false,
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+    try {
+      const url = await axios.get("http://localhost:3001/get");
+      this.setState({
+        soundUrl: url.data,
+        loaded: true,
+        disabled: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  // initialState = () => {
-  //   /** Resets the button */
+  initialState = () => {
+    /** Resets the button */
 
-  //   this.setState({
-  //     soundUrl: "",
-  //     loaded: false,
-  //     idle: true,
-  //     disabled: true,
-  //   });
-  // };
+    this.setState({
+      soundUrl: "",
+      loaded: false,
+      idle: true,
+      disabled: true,
+    });
+  };
 
-  // handleClick = async (e) => {
-  //   /** Plays the loaded sound and requests a new random sound to be played next.
-  // Disables button until current sound is finished and next sound is ready. */
+  handleClick = async (e) => {
+    /** Plays the loaded sound and requests a new random sound to be played next.
+  Disables button until current sound is finished and next sound is ready. */
 
-  //   try {
-  //     e.target.disabled = true;
-  //     const sound = new Audio(this.state.soundUrl);
-  //     await sound.play();
+    try {
+      e.target.disabled = true;
+      const sound = new Audio(this.state.soundUrl);
+      await sound.play();
 
-  //     sound.addEventListener("ended", async () => {
-  //       if (!sound.paused === false) {
-  //         try {
-  //           const url = await axios.get("http://localhost:3001/get");
-  //           this.setState({
-  //             soundUrl: url.data,
-  //             loaded: true,
-  //             disabled: false,
-  //           });
-  //           e.target.disabled = false;
-  //         } catch (error) {
-  //           console.log(error);
-  //         }
-  //       }
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+      sound.addEventListener("ended", async () => {
+        if (!sound.paused === false) {
+          try {
+            const url = await axios.get("http://localhost:3001/get");
+            this.setState({
+              soundUrl: url.data,
+              loaded: true,
+              disabled: false,
+            });
+            e.target.disabled = false;
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   render() {
     return (
@@ -94,7 +94,7 @@ class App extends React.Component {
             border: "5px solid",
             borderColor: "black",
           }}
-          // onClick={this.handleClick}
+          onClick={this.handleClick}
         >
           Play Sound
         </Button>
